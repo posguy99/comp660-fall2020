0d20320f5495452e2ae8ca0356a09b62d5b6e199
Marc Wilson <posguy99@gmail.com>
2020-09-21 20:03:47 -0700

    Add docstrings

--- src/M5_temperature.py
+++ src/M5_temperature.py
@@ -1,10 +1,22 @@
-
+'''
+This module contains functions for converting
+temperatures between degrees Fahrenheit
+and degrees Celsius
+'''
 def to_celsius(fahrenheit):
+    '''
+    Accepts degrees Fahrenheit
+    Returns degrees Celsius
+    '''
     celsius = (fahrenheit - 32) * 5/9
     return celsius
 
 
 def to_fahrenheit(celsius):
+    '''
+    Accepts degrees Celsius
+    Returns degrees Fahrenheit
+    '''
     fahrenheit = (celsius * 9/5) + 32
     return fahrenheit
 
