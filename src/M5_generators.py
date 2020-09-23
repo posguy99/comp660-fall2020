32694d80cda2dda429face8be7b5d7f928a45384
Marc Wilson <posguy99@gmail.com>
2020-09-23 11:46:07 -0700

    added second generator

--- src/M5_generators.py
+++ src/M5_generators.py
@@ -1,11 +1,27 @@
 # basic generator function
 
 
-def myGen(n):
-    yield n
-    yield n+1
+# def myGen(n):
+#     yield n
+#     yield n+1
 
-g = myGen(1)
-print(next(g))
-print(next(g))
-print(next(g))
+
+# g = myGen(1)
+# print(next(g))
+# print(next(g))
+# print(next(g))
+
+
+def myGen():
+    yield "These"
+    yield "words"
+    yield "come"
+    yield "one"
+    yield "at"
+    yield "a"
+    yield "time"
+
+
+gen = myGen()
+for i in range(1, 9):
+    print(next(gen))
