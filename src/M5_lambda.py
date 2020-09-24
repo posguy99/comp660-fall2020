0143ccec19177320c88e6d67fefa1e1d3454f0e0
Marc Wilson <posguy99@gmail.com>
2020-09-23 18:48:37 -0700

    Add third lambda example

--- src/M5_lambda.py
+++ src/M5_lambda.py
@@ -2,7 +2,14 @@
 double = lambda x: x * 2
 print(double(5))
 
+
 # Program to filter out only the even items from a list
 my_list = [1, 5, 4, 6, 8, 11, 3, 12]
 new_list = list(filter(lambda x: (x % 2) == 0, my_list))
+print(new_list)
+
+
+# Program to double each item in a list using map()
+my_list = [1, 5, 4, 6, 8, 10]
+new_list = list(map(lambda x: x * 2, my_list))
 print(new_list)
