21580fb06f9209603c6cdc771aa100f7838a99e4
Marc Wilson <posguy99@gmail.com>
2020-09-23 21:03:15 -0700

    M5_assignment_5 Create a lambda function that takes one argument and returns it

---
+++ src/M5_assignment_5.py
@@ -0,0 +1,15 @@
+
+# 5. Create a lambda function that takes one parameter (a) and returns it.
+
+# same() is the lambda function.  very simple.
+same = lambda a: a
+
+
+
+def main():
+    print('Test case')
+    print('same(5) is', same(5))
+
+
+if __name__ == "__main__":
+    main()
